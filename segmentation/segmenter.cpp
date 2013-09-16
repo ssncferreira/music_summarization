9a4ad741f7963a533ceaae28d1b0b89fac0c9aae
Susana <ssncferreira@gmail.com>
2013-09-16 12:11:54 +0100

    Correct results

--- segmentation/segmenter.cpp
+++ segmentation/segmenter.cpp
@@ -1,5 +1,5 @@
 #include "MarSystemManager.h"
-#include <math.h>		// mathematical operations (sqrt, pow,...)
+#include <math.h> // mathematical operations (sqrt, pow,...)
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -14,6 +14,9 @@ vector<realvec> result_vector;
 
 // Name of file to process
 string filename_matrix;
+
+// number of columns and rows
+mrs_natural matrix_size;
 
 // filename_matrix has format result/filename_matrix.dat
 void create_filename_matrix(string file_location) {
@@ -36,50 +39,18 @@ void create_filename_matrix(string file_location) {
 	filename_matrix.append("_matrix.dat");
 }
 
-void print_information(MarSystem* net) {
-	cout << "net: " << endl;
-	cout << "	- inSamples: " << net -> getctrl("mrs_natural/inSamples") << endl;
-	cout << "	- israte: " << net -> getctrl("mrs_real/israte") << endl;
-	cout << "	- onSamples: " << net -> getctrl("mrs_natural/onSamples") << endl;
-	cout << "	- osrate: " << net -> getctrl("mrs_real/osrate") << endl << endl;
-/*
-	cout << "	src: " << endl;
-	cout << "		- size: " << net -> getctrl("SoundFileSource/src/mrs_natural/size") << endl;
-	cout << "		- inSamples: " << net -> getctrl("SoundFileSource/src/mrs_natural/inSamples") << endl;
-	cout << "		- israte: " << net -> getctrl("SoundFileSource/src/mrs_real/israte") << endl;
-	cout << "		- onSamples: " << net -> getctrl("SoundFileSource/src/mrs_natural/onSamples") << endl;
-	cout << "		- osrate: " << net -> getctrl("SoundFileSource/src/mrs_real/osrate") << endl << endl;
-*/
-	cout << "	chroma: " << endl;
-	cout << "		- inSamples: " << net -> getctrl("Chroma/chroma/mrs_natural/inSamples") << endl;
-	cout << "		- israte: " << net -> getctrl("Chroma/chroma/mrs_real/israte") << endl;
-	cout << "		- onSamples: " << net -> getctrl("Chroma/chroma/mrs_natural/onSamples") << endl;
-	cout << "		- osrate: " << net -> getctrl("Chroma/chroma/mrs_real/osrate") << endl << endl;
-/*
-	cout << "	arff: " << endl;
-	cout << "		- inSamples: " << net -> getctrl("ArffFileSink/arff/mrs_natural/inSamples") << endl;
-	cout << "		- israte: " << net -> getctrl("ArffFileSink/arff/mrs_real/israte") << endl;
-	cout << "		- onSamples: " << net -> getctrl("ArffFileSink/arff/mrs_natural/onSamples") << endl;
-	cout << "		- osrate: " << net -> getctrl("ArffFileSink/arff/mrs_real/osrate") << endl << endl;
-
-	cout << "	g: " << endl;
-	cout << "		- inSamples: " << net -> getctrl("Gain/g/mrs_natural/inSamples") << endl;
-	cout << "		- israte: " << net -> getctrl("Gain/g/mrs_real/israte") << endl;
-	cout << "		- onSamples: " << net -> getctrl("Gain/g/mrs_natural/onSamples") << endl;
-	cout << "		- osrate: " << net -> getctrl("Gain/g/mrs_real/osrate") << endl << endl;*/
-}
 
 void print_result_vector() {
 	for(int i = 0; i < result_vector.size(); i++) {
 		cout << "result_vector[" << i << "]" << endl;
 		for(int x = 0; x < result_vector[i].getSize(); x++) {
-			cout << "	- " << x << " = " << result_vector[i].getValueFenced(x) << endl;
+			cout << " - " << x << " = " << result_vector[i].getValueFenced(x) << endl;
 		}
 	}
-
 }
 
 // calculate and outputs matrix to filename_matrix
+// NOT CREATING MATRIX, just printing
 void print_matrix(int vector_size) {
 
 	cout << "Creating file " << filename_matrix << "..." << endl;
@@ -89,36 +60,39 @@ void print_matrix(int vector_size) {
 
 	// every element of matrix matrix[i,j] represents the similarity between two onde-second
 	// segments of the piece at different times (euclidean distance is used)
-	mrs_real matrix[vector_size][vector_size];
+	//mrs_real matrix[vector_size][vector_size];
 
-	mrs_real result = 0;
+	mrs_natural result = 0;
 	mrs_real dot_product = 0;
 	mrs_real magnitude1 = 0;
 	mrs_real magnitude2 = 0;
-
+	
+	matrix_file << "P2" << endl << matrix_size << " " << matrix_size << endl;
+	matrix_file << "20" << endl;
+	
 	// Optimization: calculate only half-matrix, since the other half is equal
 	for(int rows = 0; rows < vector_size; rows++) {
 		for(int columns = 0; columns < vector_size; columns++) {
 			if(rows == columns) {
 				// optimization -> always equal to 0 in euclidean distance
 				// 1 in cosine distance
-				matrix[rows][columns] = 1;
+				//matrix[rows][columns] = 1;
+				result = 0;
 			}
 			else {
-				// Euclidean distance 
+				// Euclidean distance
 				/*for(int x = 0; x < result_vector[rows].getSize(); x++) {
-					result += pow(result_vector[rows].getValueFenced(x) - 
-						result_vector[columns].getValueFenced(x), 2);
+				result += pow(result_vector[rows].getValueFenced(x) -
+				result_vector[columns].getValueFenced(x), 2);
 				}
-				
 				matrix[rows][columns] = sqrt(result);*/
 
 				// Cosine distance (similarity)
 				// resulting similarity ranges from -1 meaning exactly opposite,
 				// to 1 meaning exactly the same, 0 indicates independence
 				for(int x = 0; x < result_vector[rows].getSize(); x++) {
-					dot_product += result_vector[rows].getValueFenced(x) * 
-						result_vector[columns].getValueFenced(x);
+					dot_product += result_vector[rows].getValueFenced(x) *
+					result_vector[columns].getValueFenced(x);
 					magnitude1 += pow(result_vector[rows].getValueFenced(x),2);
 					magnitude2 += pow(result_vector[columns].getValueFenced(x),2);
 				}
@@ -126,21 +100,28 @@ void print_matrix(int vector_size) {
 				magnitude2 = sqrt(magnitude2);
 				magnitude1 = magnitude1*magnitude2;
 
-				result = dot_product/magnitude1;
+				// To scale a range x0...x1 to a new range y0...y1:
+				// y = y0 + (y1 - y0) * (x - x0) / (x1 - x0)
+				// changing scale from -1 1 to 0 20
+				// 200 - x to invert color scale
+				result = 20 - (20*(dot_product/magnitude1+1) / 2);
 
-				matrix[rows][columns] = result;
+				//matrix[rows][columns] = result;
 			}
-			matrix_file << matrix[rows][columns] << " ";
+			
+			//matrix_file << matrix[rows][columns] << " ";
+			matrix_file << result << " ";
 
-			result = 0;
-			magnitude1 = 0;
-			magnitude2 = 0;
-			dot_product = 0;
+			result = 0.0;
+			magnitude1 = 0.0;
+			magnitude2 = 0.0;
+			dot_product = 0.0;
 		}
-		result = 0;
-		magnitude1 = 0;
-		magnitude2 = 0;
-		dot_product = 0;
+		
+		result = 0.0;
+		magnitude1 = 0.0;
+		magnitude2 = 0.0;
+		dot_product = 0.0;
 
 		matrix_file << endl;
 	}
@@ -151,40 +132,32 @@ void print_matrix(int vector_size) {
 void segmenter(string sfName) {
 	MarSystemManager mng;
 	MarSystem* net = mng.create("Series", "net");
-	MarSystem* parallel = mng.create("Parallel", "p");
+	/*MarSystem* parallel = mng.create("Parallel", "p");
 	MarSystem* parallel_serie1 = mng.create("Series", "s1");
 	MarSystem* parallel_serie2 = mng.create("Series", "s2");
-
+*/
 	net -> addMarSystem(mng.create("SoundFileSource", "src"));
 	net -> updctrl("SoundFileSource/src/mrs_string/filename", sfName);
 
-	parallel_serie1 -> addMarSystem(mng.create("Spectrum", "spc1"));
-	parallel_serie1 -> addMarSystem(mng.create("PowerSpectrum", "pw1"));
-	parallel_serie1 -> updctrl("PowerSpectrum/pw1/mrs_string/spectrumType", "power");
-	
+	net -> addMarSystem(mng.create("Spectrum", "spc1"));
+	net -> addMarSystem(mng.create("PowerSpectrum", "pw1"));
+	net -> updctrl("PowerSpectrum/pw1/mrs_string/spectrumType", "power");
+/*
 	parallel_serie2 -> addMarSystem(mng.create("Spectrum", "spc2"));
 	parallel_serie2 -> addMarSystem(mng.create("PowerSpectrum", "pw2"));
 	parallel_serie2 -> updctrl("PowerSpectrum/pw2/mrs_string/spectrumType", "power");
-	
+
 	parallel -> addMarSystem(parallel_serie1);
 	parallel -> addMarSystem(parallel_serie2);
-	
-	net -> addMarSystem(parallel);
-	
-	net -> addMarSystem(mng.create("StereoSpectrum", "ss"));
 
+	net -> addMarSystem(parallel);
+
+	net -> addMarSystem(mng.create("StereoSpectrum", "ss"));
+*/
 	net -> addMarSystem(mng.create("Chroma", "chroma"));
 	
 	net -> addMarSystem(mng.create("ArffFileSink", "arff"));
 	net -> updctrl("ArffFileSink/arff/mrs_string/filename", "output.arff");
-
-	// mrs_real/israte: rate (number of samples per second) of input
-	mrs_natural israte = net -> getctrl("mrs_real/israte") -> to<mrs_real>();
-
-	// Associate each one-second segment window of the musical piece
-	// number of input samples must be equivalent at number of samples per second (israte)
-	// multiplied by 2 (2 channels) - NECESSARY the multiplicaiton??? Yes, see self similarity matrix
-	net -> updctrl("mrs_natural/inSamples", israte*2);
 
 	int second = 0;
 	mrs_natural position = net -> getctrl("SoundFileSource/src/mrs_natural/pos") -> to<mrs_natural>();
@@ -198,7 +171,7 @@ void segmenter(string sfName) {
 		position = net -> getctrl("SoundFileSource/src/mrs_natural/pos") -> to<mrs_natural>();
 
 		/*const - When const is used, instruction 'delete net;' results in segmentation
-		 * fault - necessary because processedData is of type const realvec& */ 
+		* fault - necessary because processedData is of type const realvec& */
 		processedData = net -> getctrl("Chroma/chroma/mrs_realvec/processedData") -> to<mrs_realvec>();
 
 		result_vector.push_back(processedData);
@@ -206,11 +179,15 @@ void segmenter(string sfName) {
 		second++;
 	}
 	
-	print_result_vector();
+	matrix_size = second;
+
+	cout << "SECONDS: " << matrix_size << endl;
+	
+	//print_result_vector();
 
 	print_matrix(result_vector.size());
 
-	//delete net;	// Segmentation fault (see instruction 'const realvec& processedData = ...)'
+	//delete net; // Segmentation fault (see instruction 'const realvec& processedData = ...)'
 }
 
 int main(int argc, const char **argv) {
